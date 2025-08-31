b2062b80e60a28883a8720f3c01b6ed77bffe7bf
Jisse Reitsma <jisse@yireo.com>
2025-08-31 16:09:05 +0200

    Allow configuring options from XML layout

---
+++ ViewModel/Options.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Loki\Flatpickr\ViewModel;
+
+use Magento\Framework\View\Element\AbstractBlock;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class Options implements ArgumentInterface
+{
+    public function toJson(AbstractBlock $block): string
+    {
+        $options = array_merge_recursive($this->getDefaultOptions(), (array)$block->getOptions());
+
+        return json_encode($options);
+    }
+
+    public function getDefaultOptions(): array
+    {
+        return [
+            'altInput' => true,
+            'altFormat' => 'Y-m-d',
+            'dateFormat' => 'Y-m-d',
+        ];
+    }
+}
