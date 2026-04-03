281cf7df19dba1a599a4d2a18f9efba89fed4661
Jisse Reitsma <jisse@yireo.com>
2026-04-03 13:08:43 +0200

    Properly set first day of week

--- ViewModel/Options.php
+++ ViewModel/Options.php
@@ -3,11 +3,18 @@ declare(strict_types=1);
 
 namespace Loki\Flatpickr\ViewModel;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\View\Element\AbstractBlock;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magento\Store\Model\ScopeInterface;
 
 class Options implements ArgumentInterface
 {
+    public function __construct(
+        private ScopeConfigInterface $scopeConfig
+    ) {
+    }
+
     public function toJson(AbstractBlock $block): string
     {
         $options = array_merge($this->getDefaultOptions(), (array)$block->getOptions());
@@ -21,6 +28,17 @@ class Options implements ArgumentInterface
             'altInput' => true,
             'altFormat' => 'Y-m-d',
             'dateFormat' => 'Y-m-d',
+            'locale' => [
+                'firstDayOfWeek' => $this->getFirstDayOfWeek(),
+            ]
         ];
+    }
+
+    private function getFirstDayOfWeek(): int
+    {
+        return (int)$this->scopeConfig->getValue(
+            'general/locale/firstday',
+            ScopeInterface::SCOPE_STORE
+        );
     }
 }
